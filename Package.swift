f84487630b61ee3df9defa31a22792fae9057925
Anthony Oliveri <oliveri@us.ibm.com>
2017-02-20 17:48:30 -0600

    Added unit test skeleton
    Also fixed SDK name in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "testSdkStandalone",
+    name: "TestSdkStandalone",
     dependencies: [
         .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-http-client-swift.git", majorVersion: 0, minor: 6),
         .Package(url: "https://github.com/Hearst-DD/ObjectMapper", majorVersion: 2, minor: 2)
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import TestSdkStandaloneTests
+
+XCTMain([
+     testCase(TestSdkStandaloneTests.allTests),
+])
